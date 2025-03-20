bfb25805fa849273f681a3a138072cc66384e6a3
MaNue1-sheeshh <mvillagracia@filamer.edu.ph>
2025-03-19 20:50:12 -0700

    Add files via upload

---
+++ Villagracia_Manuel-Gabriel_R._BSIT-2B_REPO/battleship/battleship_v1.js
@@ -0,0 +1,42 @@
+// declared variables
+var location1 = 3;
+var location2 = 5;
+var location3 = 4;
+
+// User's CUrrent Guess and hits
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+// game logic
+while (isSunk ==false) {
+    guess = prompt("Ready!, Aim!, Fire! ( Enter Number 0-6 )")
+ 
+    if (guess < 0 || guess > 6) {
+        alert("anak nang 0-6 gani mang-!")
+
+    } else {
+        guesses += 1;           
+        if (guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1 ;
+
+            if (hits == 3){
+                isSunk = true;
+                alert("ouch ang sakit napatumba mo ako!")
+
+             }else{
+                alert("Hit!")
+             }
+
+            } else{
+            alert("Miss")
+        }
+
+    }
+
+}
+
+var stats = "You Sunk my ship with " + guesses + " guesses" + " and your Accuracy is " + (3/guesses);
+
+alert(stats);
